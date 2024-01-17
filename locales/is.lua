b0b272081acb1278644f82dedbf1c3efa2971ee5
azutake <azusa_takemae@dwango.co.jp>
2024-01-18 07:42:33 +0900

    fix locale file for modified localization system

--- locales/is.lua
+++ locales/is.lua
@@ -36,10 +36,10 @@ local Translations = {
     }
 }
 
-if GetConvar('qb_locale', 'en') == 'is' then
+Locale:registerLocale(false, 'is', function()
     Lang = Locale:new({
         phrases = Translations,
         warnOnMissing = true,
         fallbackLang = Lang,
     })
-end
+end)
